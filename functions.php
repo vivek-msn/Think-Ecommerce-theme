dd8a5f64dc5d669bb771f9c46fbb1c3590017c6f
Vivek <vivek_msn@hotmail.com>
2023-11-23 17:04:06 +0530

    Adding woo commerce theme support with some more default supports like thumbnail size,single image size,row and columns

--- functions.php
+++ functions.php
@@ -22,5 +22,25 @@
             'think_ecommerce_footer_menu' => 'Think Ecommerce Footer Menu'
         )
     );
+
+    add_theme_support( 'woocommerce', array(
+        'thumbnail_image_width' => 255,
+        'single_image_width'    =>255,
+        'product_grid'          => array(
+            'default_rows'      => 10,
+            'min_rows'          => 5,
+            'max_rows'          => 10,
+            'default_columns'   => 1,
+            'min_columns'       => 1,
+            'max_columns'       => 1,
+        )
+    ) );
+    add_theme_support( 'wc-product-gallery-zoom' );
+    add_theme_support( 'wc-product-gallery-lightbox' );
+    add_theme_support( 'wc-product-gallery-slider' );
+
+    if ( ! isset( $content_width ) ) {
+        $content_width = 600;
+    }
  }
  add_action('after_setup_theme','think_ecommerce_config', 0);
