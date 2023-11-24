3354dfbca41dd8ca4866c336c6d930b82f218eba
Vivek <vivek_msn@hotmail.com>
2023-11-24 23:17:39 +0530

    Implement Modification in side bar and with the help of action hooks and filters

--- functions.php
+++ functions.php
@@ -11,7 +11,7 @@
  function think_ecommerce_scripts(){
     wp_enqueue_script( 'bootstrap-js', get_template_directory_uri().'/inc/bootstrap.min.js', array('jquery'),'5.3.2', true);
     wp_enqueue_style( 'bootstrap-css', get_template_directory_uri().'/inc/bootstrap.min.css', array(), '5.3.2', 'all' );
-    wp_enqueue_style( 'think-ecommerce-style', get_stylesheet_uri(), array(), '1.0', 'all' );
+    wp_enqueue_style( 'think-ecommerce-style', get_stylesheet_uri(), array(), filemtime( get_template_directory() . '/style.css' ), 'all' );
  }
  add_action( 'wp_enqueue_scripts', 'think_ecommerce_scripts' );
 
@@ -43,37 +43,6 @@
         $content_width = 600;
     }
  }
- add_action('after_setup_theme','think_ecommerce_config', 0);
+ add_action( 'after_setup_theme', 'think_ecommerce_config', 0 );
 
- add_action( 'woocommerce_before_main_content', 'think_ecommerce_open_container_row', 5 );
- function think_ecommerce_open_container_row(){
-    echo '<div class="container shop-content"><div class="row">';
- }
-
- add_action('woocommerce_before_main_content','think_ecommerce_close_container_row', 5);
- function think_ecommerce_close_container_row(){
-    echo '</div></div>';
- }
-
- add_action('woocommerce_before_main_content', 'think_ecommerce_add_sidebar_tags', 6);
- function think_ecommerce_add_sidebar_tags(){
-    echo '<div class="sidebar-shop col-lg-3 col-md-4 order-2 order-md-1">';
- }
-
- remove_action('woocommerce_sidebar', 'woocommerce_get_sidebar');
- add_action( 'woocommerce_before_main_content','woocommerce_get_sidebar', 7 );
-
- add_action('woocommerce_before_main_content', 'think_ecommerce_close_sidebar_tags', 8);
- function think_ecommerce_close_sidebar_tags(){
-    echo '</div>';
- }
-
- add_action('woocommerce_before_main_content','think_ecommerce_add_shop_tags', 9 );
- function think_ecommerce_add_shop_tags(){
-        echo'<div class="col-lg-9 col-md-8 order-1 order-md-2">';
- }
-
- add_action('woocommerce_before_main_content','think_ecommerce_close_shop_tags', 4 );
- function think_ecommerce_close_shop_tags(){
-        echo'</div>';
- }
+ require get_template_directory() . '/inc/wc-modifications.php';
