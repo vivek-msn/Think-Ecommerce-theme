7aba7207ce105c187f091316482b79308e6bd364
Vivek <vivek_msn@hotmail.com>
2023-12-05 18:02:19 +0530

    adding login logout and account in nav bar menu but display classes issue

--- header.php
+++ header.php
@@ -28,16 +28,36 @@
 				</div>
             <section class="top-bar">
                     <div class="container">
-                        <div class="row">
-                        <div class="brand col-md-3 col-12 col-lg-2 text-centre text-md-left">Logo</div>
-                        <div class="second-column col-md-9 col-12 col-lg-10">
-                            <div class="account col-12">
-                                <div class="cart text-end">
-                                    <a href="<?php echo wc_get_cart_url(); ?>"><span class="cart-icon"></span></a>
-                                    <span class="items"><?php echo WC()->cart->get_cart_contents_count();?></span>
-                                </div>
-                            </div>
-                            <nav class="main-menu navbar navbar-expand-md navbar-light" role="navigation">
+                    <div class="row">
+						<div class="brand col-md-3 col-12 col-lg-2 text-center text-md-left">Logo</div>
+						<div class="second-column col-md-9 col-12 col-lg-10">
+							<div class="row">
+								<?php if( class_exists( 'WooCommerce' ) ): ?>
+								<div class="acount col-12">
+									<div class="navbar-expand">
+										<ul class="navbar-nav float-left">
+											<?php if( is_user_logged_in() ) : ?>
+											<li>
+												<a href="<?php echo esc_url( get_permalink( get_option( 'woocommerce_myaccount_page_id' ) ) ) ?>" class="nav-link">My Account</a>
+											</li>
+											<li>
+												<a href="<?php echo esc_url( wp_logout_url( get_permalink( get_option( 'woocommerce_myaccount_page_id' ) ) ) ) ?>" class="nav-link">Logout</a>
+											</li>
+											<?php else: ?>
+											<li>
+												<a href="<?php echo esc_url( get_permalink( get_option( 'woocommerce_myaccount_page_id' ) ) ) ?>" class="nav-link">Login / Register</a>
+											</li>												
+											<?php endif; ?>
+										</ul>
+									</div>
+									<div class="cart text-right">
+										<a href="<?php echo wc_get_cart_url(); ?>"><span class="cart-icon"></span></a>
+										<span class="items"><?php echo WC()->cart->get_cart_contents_count(); ?></span>
+									</div>
+								</div>
+								<?php endif; ?>
+                                <div class="col-12">
+                                <nav class="main-menu navbar navbar-expand-md navbar-light" role="navigation">
                                 
                                     <!-- Brand and toggle get grouped for better mobile display -->
                                     <button class="navbar-toggler ml-auto" type="button" data-bs-toggle="collapse" data-bs-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'your-theme-slug' ); ?>">
