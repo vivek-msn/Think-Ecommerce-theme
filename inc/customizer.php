b70822ee031ed408c23ddb39cf078cbf8339fb90
Vivek <vivek_msn@hotmail.com>
2023-12-11 20:20:33 +0530

    add theme customizer functionality

---
+++ inc/customizer.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * Think Ecommerce Theme Customizer
+ * @package Think Ecommerce
+ */
+
+ function Think_Ecommerce_customizer( $wp_customize ){
+
+	// Copyright Section
+
+	$wp_customize->add_section(
+		'sec_copyright', array(
+			'title'			=> 'Copyright Settings',
+			'description'	=> 'Copyright Section'
+		)
+	);
+
+			// Field 1 - Copyright Text Box
+			$wp_customize->add_setting(
+				'set_copyright', array(
+					'type'					=> 'theme_mod',
+					'default'				=> '',
+					'sanitize_callback'		=> 'sanitize_text_field'
+				)
+			);
+
+			$wp_customize->add_control(
+				'set_copyright', array(
+					'label'			=> 'Copyright',
+					'description'	=> 'Please, add your copyright information here',
+					'section'		=> 'sec_copyright',
+					'type'			=> 'text'
+				)
+			);
+
+}
+add_action( 'customize_register', 'Think_ecommerce_customizer' );
+?>
