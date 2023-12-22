5cceed641e708bf25c6dac686299a5ef5e35f74f
Vivek <vivek_msn@hotmail.com>
2023-12-22 18:14:36 +0530

    adding search form and 404 error page

--- functions.php
+++ functions.php
@@ -83,6 +83,8 @@
         if ( ! isset( $content_width ) ) {
             $content_width = 600;
         }
+
+        add_theme_support( 'title-tag' );
  }
  add_action( 'after_setup_theme', 'think_ecommerce_config', 0 );
 
